edf0b39f6211d117c5887ed1010650bb3764c509
ChloeArd <chloe.ardoise@gmail.com>
2020-12-13 19:21:05 +0100

    Jeu des devinettes nombres

--- app.js
+++ app.js
@@ -6,37 +6,38 @@ let random = Math.random() * (100 - 1) + 1;
 random = Math.trunc(random);
 console.log(random);
 
+let nbEssais = 0;
+
 function bouton () {
 
     let input1 = document.getElementById("input1").value;
     input1 = parseInt(input1);
+    nbEssais++;
 
     if (input1 > random) {
-        position("red", "Tu es haut");
+        alert("Tu es haut !");
     }
-    else if (input1 < random) {
-        position("red", "Tu es bas");
+    if (input1 < random) {
+        alert("Tu es bas !");
     }
-    else if (isNaN(input1)) {
-        position("red", "Ce n'est pas un chiffre");
+    if (isNaN(input1)) {
+        alert("T'as cru m'avoir ? Ce n'est pas un chiffre !");
     }
-    else {
-        position("green", "Tu as trouvé le bon nombre !");
+    if (input1 === random) {
+        alert("Tu as trouver le bon nombre, en " + nbEssais + " essais, BRAVOOO !");
+        location.reload();
+    }
+    if (nbEssais === 10) {
+        alert("Ahah Loupé ! Le nombre correct était " + random + " lol.");
+        location.reload();
     }
 
     for (let i = 0; i < input1; i++) {
         let nombreDit = document.createElement('li');
         nombreDit.innerHTML = "Nombre déjà utilisé: " + document.getElementById("input1").value;
-        nombreDit.style.color = "blue"
+        nombreDit.style.color = "grey";
         repeter.item(i).append(nombreDit);
     }
 }
 
 button.addEventListener("click", bouton);
-
-function position (color, text) {
-    let position = document.createElement('p');
-    position.innerHTML = text;
-    position.style.color = color;
-    donner.append(position);
-}
